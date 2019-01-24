8d52a214da9478c2ec07167927ab8bbfb0bf90e2
Danielius <danielius@cart.lt>
2019-01-24 20:00:23 +0200

    instructions as objects fix

--- src/SetInstruction.php
+++ src/SetInstruction.php
@@ -26,16 +26,4 @@ class SetInstruction {
      * @var string
      */
     public $section_id;
-
-    public function __construct(
-        int $timestamp = null,
-        $value = null,
-        string $type = null,
-        string $section_id = null
-    ) {
-        $this->timestamp = $timestamp;
-        $this->value = $value;
-        $this->type = $type;
-        $this->section_id = $section_id;
-    }
 }
